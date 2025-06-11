72e75724335cf0fd62e5de54d91acb7f12f83cf3
akshatp1811 <akshatpanday28@gmail.com>
2025-06-12 02:43:27 +0530

    Stack Implementation

---
+++ Stacks and Queues/stacks.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class stacks {
+
+    int size = 10;
+    int[] stack = new int[size];
+    int tos = -1;
+
+    void push(int val){
+        isFull(stack){
+            System.out.println("Stack OverFLow");
+            return
+        }
+        tos++;
+        stack[tos] = value
+     }
+}
