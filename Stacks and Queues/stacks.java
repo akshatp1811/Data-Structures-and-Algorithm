5d5c47036d5f5174037209efa09334c5a761bdf8
akshatp1811 <akshatpanday28@gmail.com>
2025-06-23 23:41:22 +0530

    New Commit
    New Commit

--- Stacks and Queues/stacks.java
+++ Stacks and Queues/stacks.java
@@ -1,16 +1,87 @@
-import java.util.*;
 public class stacks {
+    static int stack[] = new int[5];
+    static int tos = -1;
 
-    int size = 10;
-    int[] stack = new int[size];
-    int tos = -1;
-
-    void push(int val){
-        isFull(stack){
+    static void push(int val , int[] stack)
+    {
+        if(isFull(stack)) {
             System.out.println("Stack OverFLow");
-            return
+            return;
         }
         tos++;
-        stack[tos] = value
-     }
+        stack[tos] = val;
+    }
+
+    static int pop(int[] stack)
+    {
+        if(isEmpty(stack)) {
+            System.out.println("Stack UnderFlow");
+            return -1;
+        }
+        int value =stack[tos];
+        tos--;
+        return value;
+        
+    }
+    static int peek(int[] stack) {
+        if (isEmpty(stack)) {
+            System.out.println("Stack is empty");
+            return -1;
+        }
+        return stack[tos];
+    }
+
+    static boolean isFull(int[] stack)
+    {
+        if (tos == stack.length - 1) {
+            return true;
+        }
+        else return false;
+    }
+    static boolean isEmpty(int[] stack)
+    {
+        if (tos == -1) {
+            return true;
+        }
+        else return false;
+    }
+    static void printStack(int[] stack) {
+        if (isEmpty(stack)) {
+            System.out.println("Stack is empty");
+            return;
+        }
+        System.out.print("Stack elements: ");
+        for (int i = 0; i <= tos; i++) {
+            System.out.print(stack[i] + " ");
+        }
+        System.out.println();
+    }
+
+
+    public static void main(String[] args) {
+        push(5 , stack);
+        printStack(stack);
+        push(4 , stack);
+        printStack(stack);
+        push(3 , stack);
+        printStack(stack);
+        push(2 , stack);
+        printStack(stack);
+        push(1 , stack);
+        printStack(stack);
+        System.out.println(pop(stack));
+        printStack(stack);
+        System.out.println(pop(stack));
+        printStack(stack);
+        System.out.println(pop(stack));
+        printStack(stack);
+        System.out.println(pop(stack));
+        printStack(stack);
+        System.out.println(pop(stack));
+        printStack(stack);
+        
+
+
+        
+    }
 }
