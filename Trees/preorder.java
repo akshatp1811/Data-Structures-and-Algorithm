e5c5e1a994aa08731b78eb790602a0e95e2b9de4
akshatp1811 <akshatpanday28@gmail.com>
2025-07-03 00:00:00 +0530

    c

---
+++ Trees/preorder.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class preoreder {
+    List<Integer> list = new ArrayList<>(); 
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        if (root == null) {
+            return list; 
+        }
+        list.add(root.val);   
+
+        preorderTraversal(root.left);                 
+        preorderTraversal(root.right);     
+        return list;
+    }
+}
